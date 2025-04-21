dec960fa8793555fcf62b3baf48481869a7bb39d
weicrepower <cielchang0324@crepowermay.com>
2025-04-21 15:04:51 +0800

    feat: 🎸 update server port

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    port: 5073,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
